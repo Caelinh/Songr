d7eb56bf46984f70d37362c52ef8c6fd7ece88c1
Caelinh <Caelinhargrave@gmail.com>
2022-08-29 17:52:58 -0500

    initial commit from Spring with a little done already

---
+++ src/test/java/com/Songr/app/songr/SongrApplicationTests.java
@@ -0,0 +1,14 @@
+package com.Songr.app.songr;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
